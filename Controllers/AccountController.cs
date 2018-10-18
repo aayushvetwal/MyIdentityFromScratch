d453c53f06da781b430ae163450449ef7373315b
Aayush Vetwal <avetwal@sivicsolutionsgroup.com>
2018-10-18 17:45:55 -0400

    Initial

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserMgr => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+        public SignInManager<IdentityUser, string> SigninMgr => HttpContext.GetOwinContext().Get<SignInManager<IdentityUser, string>>();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model) {
+
+            var user = new IdentityUser()
+            {
+                UserName = model.Email,
+                PasswordHash = model.Password
+            };
+            var result = UserMgr.Create<IdentityUser, string>(user);
+
+            if (result.Succeeded)
+            {
+                return Content("User created");
+            }
+            AddErrors(result);
+
+            return View(model);
+        }
+
+
+
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+        
+        #endregion
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Sample.Startup))]
+
+namespace Sample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new IdentityDbContext("DefaultConnection"));
+            app.CreatePerOwinContext<UserStore<IdentityUser>>((opt, ctx) => new UserStore<IdentityUser>(ctx.Get<IdentityDbContext>()));
+            app.CreatePerOwinContext<UserManager<IdentityUser>>((opt, ctx) => new UserManager<IdentityUser>(ctx.Get<UserStore<IdentityUser>>()));
+            app.CreatePerOwinContext<SignInManager<IdentityUser, string>>((opt, ctx) => new SignInManager<IdentityUser, string>(ctx.Get<UserManager<IdentityUser>>(), ctx.Authentication));
+        }
+    }
+}
